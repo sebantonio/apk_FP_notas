9042dd63ea08dd87348e20d29d93b43c8fa3b50b
Sebantonio <sebantonio@gmail.com>
2026-05-15 09:32:12 +0200

    2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  distDir: 'www',
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
