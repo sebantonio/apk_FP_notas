be7ceecc10912f03acbe22d5f9bf689a826cdfbe
Sebantonio <sebantonio@gmail.com>
2026-05-15 17:07:07 +0200

    Fix Capacitor web assets directory

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.apkesonotas',
   appName: 'apkFPnotas',
-  webDir: 'www'
+  webDir: 'public'
 };
 
 export default config;
